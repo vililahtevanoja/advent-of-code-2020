f1dafa6ca31789db9c76cf1049550ed7ddb11a98
Vili Lähtevänoja <vili.lahtevanoja@netlight.com>
2020-12-02 22:07:39 +0200

    fix indentation and simplify 2nd day haskell

--- 01/01.hs
+++ 01/01.hs
@@ -1,6 +1,6 @@
 main :: IO ()
 main = do
-    inputs <- lines <$> readFile "input.txt"
-    let ns = map read inputs :: [Int]
-    let result = head [a*b*c | a <- ns, b <- ns, c <- ns, a+b+c == 2020]
-    print result
+  inputs <- lines <$> readFile "input.txt"
+  let ns = map read inputs :: [Int]
+  let result = head [a * b * c | a <- ns, b <- ns, c <- ns, a + b + c == 2020]
+  print result
