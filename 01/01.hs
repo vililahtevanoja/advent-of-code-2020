a250702607c0a3cfed1a4e7047994ff0c72c465f
Vili Lähtevänoja <vili.lahtevanoja@gmail.com>
2020-12-06 14:19:16 +0200

    Refactor

--- 01/01.hs
+++ 01/01.hs
@@ -2,5 +2,7 @@ main :: IO ()
 main = do
   inputs <- lines <$> readFile "input.txt"
   let ns = map read inputs :: [Int]
-  let result = head [a * b * c | a <- ns, b <- ns, c <- ns, a + b + c == 2020]
-  print result
+  let result1 = head [a * b | a <- ns, b <- ns, a + b == 2020]
+  let result2 = head [a * b * c | a <- ns, b <- ns, c <- ns, a + b + c == 2020]
+  putStrLn $ "Part 1: " ++ show result1
+  putStrLn $ "Part 2: " ++ show result2
